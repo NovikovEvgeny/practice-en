857a9bf29b2ee4eb9a568a6550fc000960f944d9
NovikovEvgeny <epn94@mail.ru>
2016-03-11 01:22:29 +0300

    Delete DataBase.java

--- DataBase.java
+++
@@ -1,7 +0,0 @@
-
-public class DataBase {
-	public ListDevices devices = new ListDevices();
-	public ListClients clients = new ListClients();
-	public ListChecks checks = new ListChecks();
-	public View display = new View();
-}
