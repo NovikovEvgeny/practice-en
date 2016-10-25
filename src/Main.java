54bd0781a30839460375ce07cb23c553b362d3f4
nep <nep@NBNEP.softwerke.local>
2016-10-25 11:11:41 +0300

    last commit

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
             MainController c = new MainController();
             c.run();
-        //test
+            //test
+            //last test
     }
 }
